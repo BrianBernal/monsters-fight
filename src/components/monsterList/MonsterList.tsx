7e5aaa7ba663bd85135ea98c1fdbd163c06c40a1
Brian Bernal <brianbernal27@gmail.com>
2023-04-04 15:15:16 -0500

    Add monsters from redux store

--- src/components/monsterList/MonsterList.tsx
+++ src/components/monsterList/MonsterList.tsx
@@ -1,15 +1,26 @@
-// styles
+// redux
 import { useAppSelector } from "@/redux/hooks";
+
+// styles
 import "./monsterList.scss";
 import reactLogo from "@/assets/react.svg";
 
 function MonsterList() {
   const monsters = useAppSelector((state) => state.monsters.list);
+
   return (
-    <div className="box card">
-      <img src={reactLogo} className="card__img" alt="React logo" />
-      <span>{monsters[0].name}</span>
-    </div>
+    <section className="monster-list">
+      {monsters.map((monster) => (
+        <div key={monster.id} className="box monster-list__card">
+          <img
+            src={reactLogo}
+            className="monster-list__card__img"
+            alt="React logo"
+          />
+          <span>{monster.name}</span>
+        </div>
+      ))}
+    </section>
   );
 }
 
