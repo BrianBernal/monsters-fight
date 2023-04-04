190d80f29fd700928ff9be17d7c505b1749e0c4c
Brian Bernal <brianbernal27@gmail.com>
2023-04-04 16:17:57 -0500

    Add random computer opponent

--- src/redux/monsters/monstersSlice.ts
+++ src/redux/monsters/monstersSlice.ts
@@ -4,6 +4,7 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 // redux
 import { RootState } from "../store";
 import initialState from "./initialState.d";
+import { getRandomInt } from "@/utils/math";
 
 const monsterSlice = createSlice({
   name: "monsters",
@@ -13,27 +14,25 @@ const monsterSlice = createSlice({
     // Use the PayloadAction type to declare the contents of `action.payload`
     setPlayerMonsterId: (state, action: PayloadAction<string>) => {
       state.playerMonsterId = action.payload;
-    },
-    setComputerMonsterId: (state, action: PayloadAction<string>) => {
-      state.computerMonsterId = action.payload;
+      const randomIndex = getRandomInt(state.list.length);
+      state.computerMonsterId = state.list[randomIndex].id;
     },
   },
 });
 
-export default monsterSlice.reducer;
-export { monsterSlice };
-export const { setComputerMonsterId, setPlayerMonsterId } =
-  monsterSlice.actions;
-
 // selectors:
 // Other code such as selectors can use the imported `RootState` type
-export const selectedPlayerMonsterId = ({ monsters }: RootState) => {
+const selectedPlayerMonsterId = ({ monsters }: RootState) => {
   return monsters.list.find(
     (monster) => monster.id === monsters.playerMonsterId
   );
 };
-export const selectedComputerMonsterId = ({ monsters }: RootState) => {
+const selectedComputerMonsterId = ({ monsters }: RootState) => {
   return monsters.list.find(
     (monster) => monster.id === monsters.computerMonsterId
   );
 };
+
+export default monsterSlice.reducer;
+export const { setPlayerMonsterId } = monsterSlice.actions;
+export { selectedPlayerMonsterId, selectedComputerMonsterId };
