450b6f8fb43114e08d072fb1023e3d7af14ee4fd
Brian Bernal <brianbernal27@gmail.com>
2023-04-18 17:46:12 -0500

    Backend: remove body parser and improve error management

--- backend/main.js
+++ backend/main.js
@@ -1,18 +1,17 @@
 import express from "express";
 import monsters from "./initialState.js";
-import bodyParser from "body-parser";
 
 const app = express();
 const port = 4000;
 
-let allowCrossDomain = function (_req, res, next) {
+function allowCrossDomain(_req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
   next();
 };
-app.use(allowCrossDomain);
 
-const jsonParser = bodyParser.json();
+app.use(allowCrossDomain);
+app.use(express.json())
 
 app.get("/", (_req, res) => {
   res.send("Yes, I am the server and I am alive!");
@@ -22,11 +21,13 @@ app.get("/getMonsters", (_req, res) => {
   res.json({ data: monsters, ok: true });
 });
 
-app.post("/getWinner", jsonParser, (req, res) => {
+app.post("/getWinner", (req, res) => {
   const { playerMonsterId = "", computerMonsterId = "" } = req.body;
   const areSameIds = playerMonsterId === computerMonsterId;
   if (!playerMonsterId || !computerMonsterId || areSameIds) {
-    res.status(402).send({ error: "Error in params" });
+    res.status(400).json({ error: "Error in params" });
+    console.log('El return es necesario');
+    return
   }
   const fighters = [playerMonsterId, computerMonsterId];
   const randomWinner = Math.floor(Math.random() * 2);
@@ -41,6 +42,11 @@ app.post("/getWinner", jsonParser, (req, res) => {
     },
   });
 });
+
+app.use((req, res) => {
+  console.log('Error 404 at: ', req.path)
+  res.status(404).json({error: 'Not found!'})
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
